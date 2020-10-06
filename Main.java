f8f7b1b4d74d8ed6d2e5f2cca7c9fa18da06d3fc
korks2931 <korks2931@wrdsb.ca>
2020-10-06 16:27:06 +0000

    finished a5q2

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Sevde
  */
 public class Main {
 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input 
+    Scanner input = new Scanner(System.in);
+    // ask the user to enter an integer
+    System.out.println("Please enter a positive integer");
+    int integerUser = input.nextInt();
+    // create a for loop 
+    for(int i = 1; i <= integerUser; i++){
+      // print out the count to the screen
+      System.out.print(i);
+      // create an if statement for ,
+      if (i < integerUser){
+        // print it out to the screen
+      System.out.print(",");
+      }
+    }
     
   }
 }
